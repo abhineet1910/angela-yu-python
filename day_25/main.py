440f2c85c0662d2803e4a950ddb3f9fe337b4451
abhineet1910 <abhineet1910mitra@gmail.com>
2026-06-13 00:26:23 +0530

    Angela Day 27: exploring turtle in python

--- day_25/main.py
+++ day_25/main.py
@@ -44,25 +44,25 @@
 # print(data)
 # print(data["score"])
 # print(data[data.score==data.score.max()])
-import pandas
-import pandas as pd
-data = pd.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
-# print(data["Primary Fur Color"])
-count_gray =(data['Primary Fur Color'] == 'Gray').sum()
-count_red = (data['Primary Fur Color'] == 'Cinnamon').sum()
-count_black =(data['Primary Fur Color'] == 'Black').sum()
-# one more way to do that ia
-gray = len(data[data['Primary Fur Color'] == 'Gray'])
-print(gray)
-# print(count_gray)
-# print(count_red)
-# print(count_black)
-colour_data = {
-    "Fur colour":("Gray","Cinnamon","Black"),
-    "count":(count_gray,count_red,count_black)
-}
-data = pandas.DataFrame(colour_data)
-# print(data)
-data.to_csv('squiral_colour.csv',index=False)
-colour = pd.read_csv("squiral_colour.csv")
-print(colour)
+# import pandas
+# import pandas as pd
+# data = pd.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+# # print(data["Primary Fur Color"])
+# count_gray =(data['Primary Fur Color'] == 'Gray').sum()
+# count_red = (data['Primary Fur Color'] == 'Cinnamon').sum()
+# count_black =(data['Primary Fur Color'] == 'Black').sum()
+# # one more way to do that ia
+# gray = len(data[data['Primary Fur Color'] == 'Gray'])
+# print(gray)
+# # print(count_gray)
+# # print(count_red)
+# # print(count_black)
+# colour_data = {
+#     "Fur colour":("Gray","Cinnamon","Black"),
+#     "count":(count_gray,count_red,count_black)
+# }
+# data = pandas.DataFrame(colour_data)
+# # print(data)
+# data.to_csv('squiral_colour.csv',index=False)
+# colour = pd.read_csv("squiral_colour.csv")
+# print(colour)

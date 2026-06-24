5351de98ee6f49dba13d40fef22002c095f52213
abhineet1910 <abhineet1910mitra@gmail.com>
2026-06-25 00:42:38 +0530

    git commit -m "feat: complete stock news alert logic with API integration"

--- stock-news-hard-start/stock-news-hard-start/main.py
+++ stock-news-hard-start/stock-news-hard-start/main.py
@@ -22,7 +22,6 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 load_dotenv()
 account_sid = os.getenv("TWILIO_ACCOUNT_SID")
 auth_token = os.getenv("TWILIO_AUTH_TOKEN")
-## STEP 1: Use https://newsapi.org/docs/endpoints/everything
 
 apikey = os.getenv("ALPHA_API_KEY")
 url = "https://www.alphavantage.co/query?"
@@ -57,19 +56,8 @@ def get_stckprice():
 
 
 
-        # content = get_news()
-        # print(content)
 
 
-# When STurl = https://www.alphavantage.co/query?OCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-#HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
-#HINT 2: Work out the value of 5% of yerstday's closing stock price. 
-
-
-
-## STEP 2: Use https://newsapi.org/docs/endpoints/everything
-# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
-#HINT 1: Think about using the Python Slice Operator
 new_api_key = os.getenv("NEWS_API_KEY")
 news_parameters={
     "apiKey": new_api_key,
@@ -124,27 +112,4 @@ if abs(diff) > 2:
     except Exception as e:
         print(f"Failed to send SMS via Twilio: {e}")
 
-
-
-
-
-
-
-
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# Send a separate message with each article's title and description to your phone number. 
-#HINT 1: Consider using a List Comprehension.
-
-
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 

3b65eeca49e17acaeb83137a2307de1626e8d046
abhineet1910 <abhineet1910mitra@gmail.com>
2026-07-28 01:31:08 +0530

    feat: implement dynamic blog rendering with Flask and Jinja templates

--- upgrade-blogs/startbootstrap-clean-blog-gh-pages/main.py
+++ upgrade-blogs/startbootstrap-clean-blog-gh-pages/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import requests
 blog_url='https://api.npoint.io/674f5423f73deab1e9a7'
 response = requests.get(blog_url)
@@ -14,9 +14,9 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+# @app.route('/contact')
+# def contact():
+#     return render_template('contact.html')
 @app.route("/post/<int:index>")
 def post(index):
     requested_post = None
@@ -24,6 +24,19 @@ def post(index):
         if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        return "<h1>Successfully sent your message</h1>"
+    return render_template("contact.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
